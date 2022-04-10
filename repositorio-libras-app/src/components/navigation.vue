12578f95319668fd236bca8fb3c6433dbb2cc12f
Beatriz Carolina Okabe <beatriz.carolina.okabe@gmail.com>
2022-04-09 21:00:43 -0400

    [COMMIT] #1 - [CARD-1] Criando o esqueleto do repositorio-libras-app

--- repositorio-libras-app/src/components/navigation.vue
+++ repositorio-libras-app/src/components/navigation.vue
@@ -1,11 +1,38 @@
 <template>
   <header>
-      <nav class="container"></nav>
+      <nav class="container">
+          <div class="branding">
+              <router-link class="header" :to="{name: 'home' }">RepositorioLibras<router-link>
+            </div>
+            <div class="branding">
+              <ul>
+                <router-links class="link" to="#">Início</router-links>
+                <router-links class="link" to="#">Blogs</router-links>
+                <router-links class="link" to="#">Craete Post</router-links>
+                <router-links class="link" to="#">Login/Register</router-links>
+              </ul>
+            </div>
+      </nav>
+      <menuIcon></menuIcon>
+      <transition name="mobile-nav"></transition>
+      <ul>
+          <router-links class="link" to="#">Início</router-links>
+          <router-links class="link" to="#">Blogs</router-links>
+          <router-links class="link" to="#">Craete Post</router-links>
+          <router-links class="link" to="#">Login/Register</router-links>
+      </ul>
   </header>
 </template>
 
 <script>
-export default {};
+import menuIcon from ".../assets/Icons/bars-regular.svg";
+
+export default {
+  name: "navigation",
+  components: {
+    menuIcon,
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
