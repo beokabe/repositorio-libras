ac54151ffa89a50ff4c563b98987fd08f47abdcd
Beatriz Carolina Okabe <beatriz.carolina.okabe@gmail.com>
2022-04-09 22:00:49 -0400

    [CARD-1] #2 - Configuração do eslint para rodar o projeto

--- repositorio-libras-app/src/components/navigation.vue
+++ repositorio-libras-app/src/components/navigation.vue
@@ -2,30 +2,31 @@
   <header>
       <nav class="container">
           <div class="branding">
-              <router-link class="header" :to="{name: 'home' }">RepositorioLibras<router-link>
+              <router-link class="header" :to="{name: 'home' }">RepositorioLibras</router-link>
             </div>
             <div class="branding">
               <ul>
-                <router-links class="link" to="#">Início</router-links>
-                <router-links class="link" to="#">Blogs</router-links>
-                <router-links class="link" to="#">Craete Post</router-links>
-                <router-links class="link" to="#">Login/Register</router-links>
+                <router-link class="link" to="#">Início</router-link>
+                <router-link class="link" to="#">Blogs</router-link>
+                <router-link class="link" to="#">Craete Post</router-link>
+                <router-link class="link" to="#">Login/Register</router-link>
               </ul>
             </div>
       </nav>
       <menuIcon></menuIcon>
       <transition name="mobile-nav"></transition>
       <ul>
-          <router-links class="link" to="#">Início</router-links>
-          <router-links class="link" to="#">Blogs</router-links>
-          <router-links class="link" to="#">Craete Post</router-links>
-          <router-links class="link" to="#">Login/Register</router-links>
+          <router-link class="link" to="#">Início</router-link>
+          <router-link class="link" to="#">Blogs</router-link>
+          <router-link class="link" to="#">Craete Post</router-link>
+          <router-link class="link" to="#">Login/Register</router-link>
       </ul>
   </header>
 </template>
 
 <script>
-import menuIcon from ".../assets/Icons/bars-regular.svg";
+
+import menuIcon from '../assets/Icons/bars-regular.svg';
 
 export default {
   name: "navigation",
